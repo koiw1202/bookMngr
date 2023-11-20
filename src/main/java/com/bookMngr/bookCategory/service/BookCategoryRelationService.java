93665879d93bc05a2613b26d1af81e4238a6e1c9
koiw1202 <koiw1202@naver.com>
2023-11-20 22:36:03 +0900

    유닛 테스트 추가

--- src/main/java/com/bookMngr/bookCategory/service/BookCategoryRelationService.java
+++ src/main/java/com/bookMngr/bookCategory/service/BookCategoryRelationService.java
@@ -4,9 +4,11 @@ import com.bookMngr.book.domain.Book;
 import com.bookMngr.bookCategory.model.BookCategoryRelationDto;
 import com.bookMngr.category.domain.Category;
 import com.bookMngr.bookCategory.repository.BookCategoryRepo;
+import com.bookMngr.common.error.ErrorHandler;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.bookMngr.bookCategory.domain.QBookCategoryRelation.bookCategoryRelation;
@@ -30,11 +32,8 @@ public class BookCategoryRelationService {
     private final JPAQueryFactory jpaQueryFactory ;
     private final BookCategoryRepo bookCategoryRepo ;
 
-    @Transactional
+    @Transactional(rollbackFor = {ErrorHandler.class, Exception.class}, propagation = Propagation.REQUIRED)
     public boolean updateBookCategoryRelation(BookCategoryRelationDto bookCategoryRelationDto) {
-
-        Book book = new Book(bookCategoryRelationDto.getBookId()) ;
-        Category category = new Category(bookCategoryRelationDto.getCategoryId()) ;
 
         long result = jpaQueryFactory.update(bookCategoryRelation)
                                     .set(bookCategoryRelation.bookCategoryRelationPK.category.categoryId, bookCategoryRelationDto.getCategoryId())
