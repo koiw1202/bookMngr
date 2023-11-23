e582afa710c19c0d0d8af9fec2730bcb7b7707bc
koiw1202 <koiw1202@naver.com>
2023-11-23 21:45:07 +0900

    빌드 설정 및 공통코드 추가

--- src/test/java/com/bookMngr/bookmngr/MemberTest.java
+++ src/test/java/com/bookMngr/bookmngr/MemberTest.java
@@ -1,16 +1,12 @@
 package com.bookMngr.bookmngr;
 
-import com.bookMngr.member.model.MemberDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static com.bookMngr.common.constant.RegExpConstants.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,13 +18,6 @@ public class MemberTest {
 
     @Test
     public void joinMember() {
-
-//        mockMvc.perform(
-//           MockMvcRequestBuilders.post("localhost:8080/v1.0.0/member")
-//                   .contentType(MediaType.APPLICATION_JSON)
-//                   .content(MemberDto.bu)
-//
-//        ) ;
 
     }
 
